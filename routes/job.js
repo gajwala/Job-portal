6da2e5e770c315d5e4e8ee929d4a6ee3ead5d854
Ruchira Balachandraiah <ruchirabalachandraiah@192.168.1.3>
2024-08-02 17:19:46 +0530

    added posted by user

--- routes/job.js
+++ routes/job.js
@@ -4,14 +4,21 @@ import {
   getAllJobs,
   getJobDetails,
   applyToJob,
+  getJobsPostedByUser,
 } from "../controllers/jobs.js";
 import { auth } from "../middleware/auth.js";
 import { checkRole } from "../middleware/checkRole.js";
 const router = express.Router();
 
-router.post("/", auth,checkRole('employer'), createJob);
+router.post("/", auth, checkRole("employer"), createJob);
 router.get("/", auth, getAllJobs);
 router.get("/:jobId", auth, getJobDetails);
-router.post("/apply", auth,checkRole('freelancer'), applyToJob);
+router.get(
+  "/postedBy/:userId",
+  auth,
+  checkRole("employer"),
+  getJobsPostedByUser
+);
+router.post("/apply", auth, checkRole("freelancer"), applyToJob);
 
 export default router;
