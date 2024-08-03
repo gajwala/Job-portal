8025e3f10526f262b86de7b6392ef1dfb4c53a83
Ruchira Balachandraiah <ruchirabalachandraiah@192.168.1.2>
2024-08-03 21:27:24 +0530

    added get user details APi

--- controllers/user.js
+++ controllers/user.js
@@ -84,3 +84,17 @@ export const updateUserProfile = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const getUserDetails = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { login, register, updateUserProfile } from "../controllers/user.js";
+import {
+  login,
+  register,
+  updateUserProfile,
+  getUserDetails,
+} from "../controllers/user.js";
 import { auth } from "../middleware/auth.js";
 
 const router = express.Router();
@@ -7,5 +12,6 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.patch("/:id", auth, updateUserProfile);
+router.get("/:id", auth, getUserDetails);
 
 export default router;
